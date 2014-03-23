672203f2f6520300e3831e3ba5f51bd884fe1d6a
rikachka <rika.95@mail.ru>
2014-03-23 20:27:12 +0300

    Create toandfro.py

---
+++ 2/toandfro.py
@@ -0,0 +1,16 @@
+width = int(input())
+str = raw_input()
+heigth = len(str) // width
+str_list = [x for x in str]
+order = 1
+column = line = i = 0
+while line < heigth:
+	while column < width and column >= 0:
+		str_list[line + column*heigth] = str[i]
+		i = i + 1
+		column = column + order
+	order = 0 - order
+	column = column + order
+	line = line + 1
+new_str = ''.join(x for x in str_list)
+print(new_str)
